8980095a145b90ab3410794c96958711e437e03d
NuumanIsaev <aestheticdude21@gmail.com>
2023-04-07 11:45:47 +0600

    second commit

--- ShopApi/urls.py
+++ ShopApi/urls.py
@@ -18,10 +18,25 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 from category.views import CategoryViewSet
 from product.views import ProductViewSet
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Shop_API test",
+      default_version='v1',
+      description="test api for blog api py.26",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 router = SimpleRouter()
 router.register('categories', CategoryViewSet)
 router.register('products', ProductViewSet)
@@ -31,7 +46,11 @@ urlpatterns = [
     path('api/v1/accounts/', include('account.urls')),
     path('api/v1/orders/', include('order.urls')),
     path('api/v1/', include(router.urls)),
+    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
